73abe37ae8de375dd2ecf3240e4233663adb2686
nksteven <yangshaoli2005@gmail.com>
2020-05-11 17:56:15 +0800

    update to V2.0

--- ios/Classes/ZendeskPlugin.m
+++ ios/Classes/ZendeskPlugin.m
@@ -1,6 +1,8 @@
 #import "ZendeskPlugin.h"
 
-#import <ZDCChat/ZDCChat.h>
+#import <ChatSDK/ChatSDK.h>
+#import <ChatProvidersSDK/ChatProvidersSDK.h>
+#import <MessagingSDK/MessagingSDK.h>
 
 #define ARGB_COLOR(c) [UIColor colorWithRed:((c>>16)&0xFF)/255.0 green:((c>>8)&0xFF)/255.0 blue:((c)&0xFF)/255.0  alpha:((c>>24)&0xFF)/255.0]
 
@@ -16,7 +18,7 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"init" isEqualToString:call.method]) {
-    [ZDCChat initializeWithAccountKey:call.arguments[@"accountKey"]];
+      [ZDKChat initializeWithAccountKey:call.arguments[@"accountKey"] queue:dispatch_get_main_queue()];
     result(@(true));
   } else if ([@"setVisitorInfo" isEqualToString:call.method]) {
       NSString *email = call.arguments[@"email"];
@@ -24,62 +26,47 @@
       NSString *name = call.arguments[@"name"];
       NSString *note = call.arguments[@"note"];
 
-      [ZDCChat updateVisitor:^(ZDCVisitorInfo *user) {
-          if (![phoneNumber isKindOfClass:[NSNull class]]) {
-              user.phone = phoneNumber;
-          }
+      ZDKChatAPIConfiguration *chatAPIConfiguration = [[ZDKChatAPIConfiguration alloc] init];
+      chatAPIConfiguration.department = @"Department Name";
+      if ([phoneNumber isKindOfClass:[NSNull class]]) {
+          phoneNumber = @"";
+      }
+      if ([email isKindOfClass:[NSNull class]]) {
+          email = @"";
+      }
 
-          if (![email isKindOfClass:[NSNull class]]) {
-              user.email = email;
-          }
-
-          if (![name isKindOfClass:[NSNull class]]) {
-              user.name = name;
-          }
-          
-          if (![note isKindOfClass:[NSNull class]]) {
-              [user addNote:note];
-          }
-      }];
+      if ([name isKindOfClass:[NSNull class]]) {
+          name = @"";
+      }
+      chatAPIConfiguration.visitorInfo = [[ZDKVisitorInfo alloc] initWithName:name
+                                                                        email:email
+                                                                  phoneNumber:phoneNumber];
+      ZDKChat.instance.configuration = chatAPIConfiguration;
+      
       result(@(true));
   } else if ([@"startChat" isEqualToString:call.method]) {
-      NSNumber *navigationBarColor = call.arguments[@"iosNavigationBarColor"];
-      NSNumber *navigationTitleColor = call.arguments[@"iosNavigationTitleColor"];
-      
-      if ([navigationBarColor isKindOfClass:[NSNull class]] || [navigationTitleColor isKindOfClass:[NSNull class]]) {
-          [ZDCChat startChat:nil];
-          result(@(true));
-          return;
-      }
-      
       UINavigationController *navVc = [[UINavigationController alloc] init];
-      navVc.navigationBar.translucent = NO;
-      navVc.navigationBar.barTintColor = ARGB_COLOR([navigationBarColor integerValue]);
-      navVc.navigationBar.titleTextAttributes = @{
-                                                  NSForegroundColorAttributeName: ARGB_COLOR([navigationTitleColor integerValue])
-                                                  };
+
       
-      
-      [ZDCChat startChatIn:navVc withConfig:nil];
-      
-      UIViewController *rootVc = [UIApplication sharedApplication].keyWindow.rootViewController ;
-      [rootVc presentViewController:navVc
-                           animated:true
-                         completion:^{
-                             UIBarButtonItem *back = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Back", comment: @"")
-                                                                                      style:UIBarButtonItemStylePlain
-                                                                                     target:self
-                                                                                     action:@selector(close:)];
-                             
-                             [back setTitleTextAttributes:@{ NSForegroundColorAttributeName: ARGB_COLOR([navigationTitleColor integerValue])} forState:UIControlStateNormal];
-                             
-                             navVc.topViewController.navigationItem.leftBarButtonItem = back;
-                             
-                         }];
+      // Name for Bot messages
+      ZDKMessagingConfiguration *messagingConfiguration = [[ZDKMessagingConfiguration alloc] init];
+      messagingConfiguration.name = @"";
+
+      ZDKChatConfiguration *chatConfiguration = [[ZDKChatConfiguration alloc] init];
+      chatConfiguration.isPreChatFormEnabled = YES;
+
+      // Build view controller
+      NSError *error = nil;
+      ZDKChatEngine* chatEngine = [ZDKChatEngine engineAndReturnError: &error];
+      UIViewController *viewController = [ZDKMessaging.instance buildUIWithEngines:@[chatEngine]
+                                                                           configs:@[messagingConfiguration, chatConfiguration]
+                                                                             error:&error];
+      // Present view controller
+      [navVc pushViewController:viewController animated:YES];
 
     result(@(true));
   } else if ([@"version" isEqualToString:call.method]) {
-      result(ZDC_CHAT_SDK_VERSION);
+      result(@"SDK V2");
   } else {
     result(FlutterMethodNotImplemented);
   }
